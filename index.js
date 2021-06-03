8013a8a3ed9421362f74ab6ec1487db1689d312e
Michael Reynolds <michael4reynolds@gmail.com>
2021-06-03 10:34:23 -0400

    transforming and returning json data for an api

--- index.js
+++ index.js
@@ -6,18 +6,26 @@ addEventListener('fetch', (event) => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const resp = await fetch('https://api.unsplash.com/photos', {
-    headers: {
-      Authorization: `Client-ID ${CLIENT_ID}`,
+  const { query } = await request.json()
+
+  const resp = await fetch(
+    `https://api.unsplash.com/search/photos?query=${query}`,
+    {
+      headers: {
+        Authorization: `Client-ID ${CLIENT_ID}`,
+      },
     },
-  })
+  )
   const data = await resp.json()
-  return new Response(JSON.stringify(data), {
+  const images = data.results.map((image) => ({
+    id: image.id,
+    image: image.urls.small,
+    link: image.links.html,
+  }))
+
+  return new Response(JSON.stringify(images), {
     headers: {
       'Content-type': 'application/json',
     },
   })
-
-  const { query } = await request.json()
-  return new Response(`Your query was ${query}`)
 }
