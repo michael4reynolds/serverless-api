19e7fc240fdfb093c17e805bfda81fc99aea803b
Michael Reynolds <michael4reynolds@gmail.com>
2021-06-03 09:06:28 -0400

    make api request using fetch api

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ addEventListener('fetch', (event) => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  const CLIENT_ID = 'PwqC0i0JdupuqppRevfnD6dk3FdZVwoE_CoXIvhRQOk'
+  const resp = await fetch('https://api.unsplash.com/photos', {
+    headers: {
+      Authorization: `Client-ID ${CLIENT_ID}`,
+    },
+  })
+  const data = await resp.json()
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+
   const { query } = await request.json()
   return new Response(`Your query was ${query}`)
 }
